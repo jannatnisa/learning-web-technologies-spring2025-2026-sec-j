a2305d0cbe21ee694aeaf2ee030bc46ee4f503bc
Nisa <jnisa3529@gmail.com>
2026-04-12 17:44:40 +0600

    Add task6-b

---
+++ FinalLabTask-1/task-6/b/b.php
@@ -0,0 +1,32 @@
+<?php
+if (isset($_POST['blood_group'])) {
+$blood = $_POST['blood'];
+echo "Selected Blood Group: " . $blood;
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>task6-b</title>
+</head>
+<body>
+
+<form action="handler.php" method="post">
+    Blood Group:
+    <select name="blood">
+        <option value="A+">A+</option>
+        <option value="A-">A-</option>
+        <option value="B+">B+</option>
+        <option value="B-">B-</option>
+        <option value="O+">O+</option>
+        <option value="O-">O-</option>
+        <option value="AB+">AB+</option>
+        <option value="AB-">AB-</option>
+    </select>
+
+    <br><br>
+    <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
