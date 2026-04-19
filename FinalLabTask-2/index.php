ece32c35f0f9fefcfcbb7146722e7dfd6f58255c
Nisa <jnisa3529@gmail.com>
2026-04-19 14:57:33 +0600

    Update Index

--- FinalLabTask-2/index.php
+++ FinalLabTask-2/index.php
@@ -1,26 +1,13 @@
+<?php session_start(); ?>
+
+<h1>Welcome</h1>
+
+<a href="registration.php">Registration</a><br>
+<a href="login.php">Login</a><br>
+<a href="forgot.php">Forgot Password</a><br>
+
 <?php
-session_start();
-
-if(isset($_POST['submit'])){
-    $_SESSION['user'] = [
-        "name" => $_POST['name'],
-        "email" => $_POST['email'],
-        "password" => $_POST['password'],
-        "gender" => $_POST['gender']
-    ];
-
-    echo "Registration Successful!";
+if(isset($_COOKIE['user'])){
+    echo "Remembered User: " . $_COOKIE['user'];
 }
 ?>
-
-<form method="post">
-Name: <input type="text" name="name"><br>
-Email: <input type="email" name="email"><br>
-Password: <input type="password" name="password"><br>
-
-Gender:
-<input type="radio" name="gender" value="Male">Male
-<input type="radio" name="gender" value="Female">Female<br>
-
-<input type="submit" name="submit" value="Register">
-</form>
