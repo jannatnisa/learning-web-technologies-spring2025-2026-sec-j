3cdd7d1a0499ba5a552e19be4afa7f4545c44c9a
Nisa <jnisa3529@gmail.com>
2026-04-26 14:22:01 +0600

    Updated Login

--- FinalLabTask-3/login.php
+++ FinalLabTask-3/login.php
@@ -5,7 +5,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Simple hardcoded user (same style as your repo)
     if ($username == "admin" && $password == "1234") {
         $_SESSION['user'] = $username;
         header("Location: dashboard.php");
