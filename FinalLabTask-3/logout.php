8cdafeef8eee425ea3a1fe04d9004d0c04029ce0
Nisa <jnisa3529@gmail.com>
2026-04-26 14:24:51 +0600

    Updated Logout

--- FinalLabTask-3/logout.php
+++ FinalLabTask-3/logout.php
@@ -1,9 +1,5 @@
 <?php
 session_start();
-
-unset($_SESSION['status']);
-setcookie('status', 'true', time() - 10, '/');
-
-header('location: ../view/login.php');
-
+session_destroy();
+header("Location: login.php");
 ?>
